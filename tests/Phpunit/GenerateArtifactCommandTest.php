91006ce800e0281f231369bde1b47cab0351106e
Matthew Grasmick <matt.grasmick@gmail.com>
2018-01-10 11:38:52 -0500

    Adding example test.

---
+++ tests/Phpunit/GenerateArtifactCommandTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Grasmash\Artifice\Tests\Phpunit;
+
+class ExpanderTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testExample()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
